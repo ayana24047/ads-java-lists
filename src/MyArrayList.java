826f7a8fba8086ff95147bbb52eca933998f0dbb
ayana <ayana.marhabat@mail.ru>
2025-04-13 22:45:02 +0500

    feat: implement MyArrayList with all core methods

---
+++ src/MyArrayList.java
@@ -0,0 +1,108 @@
+import java.util.Iterator;
+
+public class MyArrayList<T> implements MyList<T> {
+    private Object[] data;
+    private int size;
+    private static final int DEFAULT_CAPACITY = 10;
+
+    public MyArrayList() {
+        data = new Object[DEFAULT_CAPACITY];
+        size = 0;
+    }
+
+    private void ensureCapacity() {
+        if (size == data.length) {
+            Object[] newData = new Object[data.length * 2];
+            for (int i = 0; i < size; i++) {
+                newData[i] = data[i];
+            }
+            data = newData;
+        }
+    }
+
+    @Override
+    public void add(T item) {
+        ensureCapacity();
+        data[size++] = item;
+    }
+
+    @Override
+    public void add(int index, T item) {
+        if (index < 0 || index > size) throw new IndexOutOfBoundsException();
+        ensureCapacity();
+        for (int i = size; i > index; i--) {
+            data[i] = data[i - 1];
+        }
+        data[index] = item;
+        size++;
+    }
+
+    @Override
+    public T get(int index) {
+        if (index < 0 || index >= size) throw new IndexOutOfBoundsException();
+        return (T) data[index];
+    }
+
+    @Override
+    public T remove(int index) {
+        if (index < 0 || index >= size) throw new IndexOutOfBoundsException();
+        T removed = (T) data[index];
+        for (int i = index; i < size - 1; i++) {
+            data[i] = data[i + 1];
+        }
+        data[--size] = null;
+        return removed;
+    }
+
+    @Override
+    public boolean remove(T item) {
+        for (int i = 0; i < size; i++) {
+            if (data[i].equals(item)) {
+                remove(i);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    @Override
+    public void clear() {
+        data = new Object[DEFAULT_CAPACITY];
+        size = 0;
+    }
+
+    @Override
+    public boolean contains(T item) {
+        for (int i = 0; i < size; i++) {
+            if (data[i].equals(item)) return true;
+        }
+        return false;
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return new Iterator<T>() {
+            private int cursor = 0;
+
+            @Override
+            public boolean hasNext() {
+                return cursor < size;
+            }
+
+            @Override
+            public T next() {
+                return (T) data[cursor++];
+            }
+        };
+    }
+}
