65f9e1d36c806ff59880a64b6886552109437fc6
ayana <ayana.marhabat@mail.ru>
2025-04-13 23:03:53 +0500

    test: finalize Main.java with MyLinkedList test

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
+        // --- Тестируем MyArrayList ---
         MyList<String> list = new MyArrayList<>();
 
         list.add("Apple");
@@ -13,6 +14,22 @@ public class Main {
         System.out.println("Size after removal: " + list.size()); // 2
 
         for (String item : list) {
+            System.out.println("Item: " + item);
+        }
+
+        // --- Тестируем MyLinkedList ---
+        System.out.println("\n--- Testing MyLinkedList ---");
+        MyList<String> linkedList = new MyLinkedList<>();
+
+        linkedList.add("One");
+        linkedList.add("Two");
+        linkedList.add("Three");
+
+        System.out.println("Get index 1: " + linkedList.get(1)); // Two
+        linkedList.remove("Two");
+        System.out.println("Size after remove: " + linkedList.size()); // 2
+
+        for (String item : linkedList) {
             System.out.println("Item: " + item);
         }
     }
